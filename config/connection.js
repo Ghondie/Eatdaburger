e79cc1fcdec0dc27e9f63e8753128900e181004b
Jared Schau <jaredschau@MacBook-Pro.local>
2020-08-18 15:44:38 -0600

    sql server changes

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,15 @@
 var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1ghondie",
-    database: "burger_db"
-});
-
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "1ghondie",
+        database: "burger_db"
+    });
+};
 // Make connection.
 // connection.connect(function (err) {
 //     if (err) {
